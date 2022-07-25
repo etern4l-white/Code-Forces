e3d1d0ffd087acf157fe0316c41a96b101fe26a8
etern4l-white <eternal.white@outlook.com>
2022-07-25 21:39:05 +0300

    Update yes_or_yes.py

--- HTU-ACM-2022/JPCP-training-1/codes/yes_or_yes.py
+++ HTU-ACM-2022/JPCP-training-1/codes/yes_or_yes.py
@@ -2,3 +2,12 @@ for i in range(int(input())):
 	print("YES" if input().lower() == "yes" else "NO")
   
 # Another method (same idea)
+"""
+
+n = int(input())
+for i in range(n):
+    l = input() 
+    print("YES" if l.lower() == "yes" else "NO")
+
+
+"""
